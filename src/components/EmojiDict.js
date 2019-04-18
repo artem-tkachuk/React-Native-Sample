ddf1776986251c57d052d2aa286e163b68ca9c04
Artem Tkachuk <artemtkachuk@yahoo.com>
2019-04-17 22:13:00 -0700

    First setup

---
+++ src/components/EmojiDict.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+class EmojiDict extends Component {
+    state = {
+        '😃': '😃 Smiley',
+        '🚀': '🚀 Rocket',
+        '⚛︎': '⚛︎ Atom symbol'
+    };
+
+    render() {
+        return (
+          <View style = {styles.container}>
+              <Text>{this.state['😃']}</Text>
+              <Text>{this.state['🚀']}</Text>
+          </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+   container: {
+       flex: 1,
+       justifyContent: 'center',
+       alignItems: 'center'
+   }
+});
+
+export default EmojiDict;
